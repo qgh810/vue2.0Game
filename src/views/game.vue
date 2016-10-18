68c3d8ef2e843cc657bf444c60dc37e88cb75eaf
邱国辉 <530784837@qq.com>
2016-10-19 01:02:49 +0800

    完成点击黑键逻辑

--- src/views/game.vue
+++ src/views/game.vue
@@ -3,8 +3,9 @@
     <table class="table">
       <tr v-for="row in rows">
         <td v-for="n in 3">
-          <div class="box" :class="{'black': n === row.selectedIndex}">
-            <div class="black-box" v-show="n === row.selectedIndex"></div>
+          <div class="box">
+            <div class="black-box" v-show="n === row.blackIndex" @touchend="next(row, n)"></div>
+            <div class="white-box" v-show="n !== row.blackIndex" @touchend="over(row, n)"></div>
           </div>
         </td>
       </tr>
@@ -17,36 +18,61 @@
 export default {
   data () {
     return {
+      // 总时间
+      allTime: 60,
       // 分数
       mark: 0,
       rows: [
         {
-          selectedIndex: 3
+          blackIndex: 3
         },
         {
-          selectedIndex: 2
+          blackIndex: 2
         },
         {
-          selectedIndex: 1
+          blackIndex: 1
         },
         {
-          selectedIndex: 2
-        },
-        {
-          selectedIndex: 2
+          blackIndex: 2
         }
-      ]
+      ],
+      lastTouchIndex: -1
     }
   },
   computed: {},
-  ready () {},
+  created () {
+  },
   attached () {},
   methods: {
+    next (row, selectedIndex) {
+      if (this.rows.indexOf(row) !== this.rows.length - 1) return
+      this.lastTouchIndex = selectedIndex
+      let newRow = {
+        blackIndex: this.random(1, 3, true)
+      }
+      this.rows.unshift(newRow)
+      this.rows.pop()
+    },
+    over (row, selectedIndex) {
+      if (this.rows.indexOf(row) !== this.rows.length - 1) return
+      this.lastTouchIndex = selectedIndex
+      console.log(row)
+      console.log(selectedIndex)
+    },
     className (index) {
       var result = {
         col1: index % 3 === 0,
         col2: index % 3 === 1,
         col3: index % 3 === 2
+      }
+      return result
+    },
+    random (min, max, isInt) {
+      if (!min) return window.Math.random()
+      var result = false
+      result = min + window.Math.random() * (max - min + 1)
+      if (isInt) {
+        result = parseInt(result)
       }
       return result
     }
@@ -61,7 +87,7 @@ export default {
     height 100%
     .table
       width 100%
-      transform translate3d(0, -25vh, 0)
+      /*transform translate3d(0, -25vh, 0)*/
       transition transform ease 1s
     .box
       display table-cell
@@ -74,4 +100,8 @@ export default {
       width 100%
       height 100%
       background #000
+    .white-box
+      width 100%
+      height 100%
+      background #fff
 </style>
